d1a0a27e84f3c0dbe7c48162098e0567c870da71
ochsec <cochsenreither@protonmail.com>
2020-12-11 14:24:30 -0700

    added Google Analytics

--- scripts/site/generate.py
+++ scripts/site/generate.py
@@ -142,6 +142,15 @@ for fund in funds:
                 </table>
             </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-576GKZKB5W"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-576GKZKB5W');
+        </script>
         </body>
         </html>
     """
