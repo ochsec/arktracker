36101c6fd6e4a8e712043401bf09d0939ef2985f
ochsec <cochsenreither@protonmail.com>
2020-12-17 11:22:22 -0700

    updated header

--- scripts/site/generate.py
+++ scripts/site/generate.py
@@ -63,40 +63,37 @@ for fund in funds:
     html += get_change_tab(day_changes_df, day_start_date_str, day_end_date_str, 1)
     html += get_change_tab(weekly_changes_df, week_start_date_str, week_end_date_str, 5)
 
-    if fund["name"] == "ARKK":
-        html += """
-            <div class="toast-container position-absolute p-3 top-0 start-50 translate-middle-x">
-                <div class="toast" data-autohide="false">
-                    <div class="toast-header">
-                    Notice
-                    </div>
-                    <div class="toast-body">
-                    This site does not use cookies, track you, or record any user data.
-                    <div class="mt-2 pt-2 border-top">
-                        <button type="button" class="btn btn-primary btn-sm" data-dismiss="toast">Okay</button>
-                    </div>
+
+    html += """
+        <div class="toast-container position-absolute p-3 top-0 start-50 translate-middle-x">
+            <div class="toast" data-autohide="false">
+                <div class="toast-header">
+                Privacy Notice
+                </div>
+                <div class="toast-body">
+                This site does not use cookies, track you, or record any user data.
+                <div class="mt-2 pt-2 border-top">
+                    <button type="button" id="toast-btn" class="btn btn-primary btn-sm" data-dismiss="toast">Okay</button>
                 </div>
             </div>
-        """
-
-    html += """
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-    """
-
-    if fund["name"] == "ARKK":
-        html += """
-            <script>
-            $(document).ready(function(){
-                $('.toast').toast('show');
+        <script src="https://use.fontawesome.com/f9db0cd5da.js"></script>
+        <script>
+        $(document).ready(function(){
+            $('#privacy').click(function() {
+                $('.toast').toast('show');    
             });
-            </script>
-            """
 
-    html += """
-        </body>
-        </html>
-        """
+            $('#toast-btn').click(function() {
+                $('.toast').toast('hide');
+            });
+        });
+        </script>
+    </body>
+    </html>
+    """
 
     file = open(f"""../site/{fund["page_name"]}""", "w")
     file.write(html)
