8b4f42813ef0f643ae3bedfc4a28df99430a35ca
ochsec <cochsenreither@protonmail.com>
2020-12-17 16:54:39 -0700

    added line chart example

--- scripts/site/generate_header.py
+++ scripts/site/generate_header.py
@@ -1,7 +1,7 @@
 def get_header():
     return """
     <!DOCTYPE html>
-    <html>
+    <html lang="en">
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
