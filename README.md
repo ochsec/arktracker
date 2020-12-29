5c57ece0b59fb9d0d35cc534f3dcf9b18991d1e4
ochsec <cochsenreither@protonmail.com>
2020-12-28 20:07:45 -0700

    ditch site for notebooks

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # arktracker.info
-New project to track performance and holdings changes in the ARK universe of funds: ark-funds.com
-
-Website: https://arktracker.info/
+A collection of Jupyter notebooks that tracks the performance and holdings changes in the ARK universe of funds: ark-funds.com
 
 All data collected was made publicly available by ARK Investment Management LLC. As data is collected, the goal of the site is to track changes to the holdings of the funds for these purposes:
 1. Gain insight into stock-picking methodology
 2. Measure fund stock-picking performance
 3. Discover growth stocks discovered by ARK
-
--- notebooks/funds.py
+++ notebooks/funds.py
@@ -1,45 +1,31 @@
-funds = [
-    {
-        "name": "ARKF",
+funds = {
+    "ARKF": {
         "full_name": "ARKF / ARK FINTECH INNOVATION ETF",
-        "page_name": "arkf.html",
         "csv_url": "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_FINTECH_INNOVATION_ETF_ARKF_HOLDINGS.csv"
     },
-    {
-        "name": "ARKG",
+    "ARKG": {
         "full_name": "ARKG / ARK Genomic Revolution ETF",
-        "page_name": "arkg.html",
         "csv_url": "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_GENOMIC_REVOLUTION_MULTISECTOR_ETF_ARKG_HOLDINGS.csv"
     },
-    {
-        "name": "ARKK",
+    "ARKK": {
         "full_name": "ARKK / ARK INNOVATION ETF",
-        "page_name": "arkk.html",
         "csv_url": "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"
     },
-    {
-        "name": "ARKQ",
+    "ARKQ": {
         "full_name": "ARKQ / ARK Autonomous Technology & Robotics ETF",
-        "page_name": "arkq.html",
         "csv_url": "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_AUTONOMOUS_TECHNOLOGY_&_ROBOTICS_ETF_ARKQ_HOLDINGS.csv"
     },
-    {
-        "name": "ARKW",
+    "ARKW": {
         "full_name": "ARKW / ARK Next Generation Internet ETF",
-        "page_name": "arkw.html",
         "csv_url": "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_NEXT_GENERATION_INTERNET_ETF_ARKW_HOLDINGS.csv"
     },
-    {
-        "name": "IZRL",
+    "IZRL": {
         "full_name": "IZRL / Israel Innovative Technology ETF",
-        "page_name": "izrl.html",
         "csv_url": "https://ark-funds.com/wp-content/fundsiteliterature/csv/ARK_ISRAEL_INNOVATIVE_TECHNOLOGY_ETF_IZRL_HOLDINGS.csv"
     },
-    {
-        "name": "PRNT",
+    "PRNT": {
         "full_name": "PRNT / The 3D Printing ETF",
-        "page_name": "prnt.html",
         "csv_url": "https://ark-funds.com/wp-content/fundsiteliterature/csv/THE_3D_PRINTING_ETF_PRNT_HOLDINGS.csv"
     }
-]
+}
 
